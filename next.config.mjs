d6f1f43447f5140622c51be9ad413debfef00f45
Uri Frischman <uri@thevets.com>
2023-10-19 23:33:17 +0300

    WIP

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,24 @@
  * for Docker builds.
  */
 await import("./src/env.mjs");
+import webpack from 'webpack';
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    webpack: (webpackConfig, { webpack }) => {
+        webpackConfig.plugins.push(
+            // Remove node: from import specifiers, because Next.js does not yet support node: scheme
+            // https://github.com/vercel/next.js/issues/28774
+            new webpack.NormalModuleReplacementPlugin(
+                /^node:/,
+                (resource : any) => {
+                    resource.request = resource.request.replace(/^node:/, '');
+                },
+            ),
+        );
+
+        return webpackConfig;
+    },
+};
 
 export default config;
