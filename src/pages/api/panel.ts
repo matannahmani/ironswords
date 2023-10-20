55c81c0403ab0e5fd0adbc9bd2158aaccabfbc84
Uri Frischman <uri@thevets.com>
2023-10-20 15:21:41 +0300

    edits

--- src/pages/api/panel.ts
+++ src/pages/api/panel.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { renderTrpcPanel } from "trpc-panel";
 import { appRouter } from "@/server/api/root";
+import {getUrl} from "@/trpc/shared";
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
     res.status(200).send(
         renderTrpcPanel(appRouter, {
-            url: "http://localhost:3000/api/trpc",
+            url: getUrl(),
             transformer: "superjson",
         })
     );
