0d9f815efd19a134be2ef6a87630f5f51b0f4b83
Matan Nahmani <matanmatan999new@gmail.com>
2023-10-29 14:33:16 +0900

    merge warehouse-dashboard

--- src/app/warehouses/components/data-table-toolbar.tsx
+++ src/app/warehouses/components/data-table-toolbar.tsx
@@ -7,7 +7,6 @@ import { Button } from "@ui/button";
 import { Input } from "@ui/input";
 import { DataTableViewOptions } from "./data-table-view-options";
 
-import { ticketPriority, ticketStatus } from "../data/data";
 import { DataTableFacetedFilter } from "./data-table-faceted-filter";
 
 interface DataTableToolbarProps<TData> {
